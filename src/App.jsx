5d6f00088d15ffd6d48c5d4e0a55e119e173a540
PooraniVicky <poorani2475@gmail.com>
2024-06-17 14:47:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage';
+import SignUpPage from './Pages/SignUpPage';
+import ForgotPasswordPage from './Pages/ForgetPasswordPage';
+import ResetPasswordPage from './Pages/ResetPasswordPage';
+import HomePage from './Pages/HomePage';
+
+
+const App = () => {
+  const isAuthenticated = () => {
+    // Example: Check if auth token is present in local storage
+    return !!localStorage.getItem('token');
+  };
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+          <Route
+            path="/home"
+            element={isAuthenticated() ? <HomePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/Pages/ResetPasswordPage.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ResetPasswordPage = () => {
+  const { token } = useParams();
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  const handleResetPassword = async () => {
+    if (password !== repeatPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+    try {
+      await axios.post(`https://password-reset-backend-ud2r.onrender.com/auth/reset-password/${token}`, { password, repeatPassword });
+      alert('Password reset completed');
+    } catch (error) {
+      alert('Error resetting password');
+      console.error('Error resetting password:', error);
+    }
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className='row d-flex justify-content-center'>
+        <div className='col-sm-12 col-md-8 col-lg-6 col-xl-6'>
+          <div className="container reset-page mt-5 px-4 py-4">
+            <div className='row d-flex justify-content-center'>
+              <div className='col-sm-10 col-md-8 col-lg-8 col-xl-8'>
+                <h3 className="text-center">Reset Password</h3><br />
+                <form onSubmit={(e) => e.preventDefault()}>
+                  <div className="mb-3">
+                    <label className="form-label">New Password</label>
+                    <input
+                      type="password"
+                      className="form-control newpassword"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Confirm New Password</label>
+                    <input
+                      type="password"
+                      className="form-control confirm-password"
+                      value={repeatPassword}
+                      onChange={(e) => setRepeatPassword(e.target.value)}
+                      required
+                    />
+                  </div><br />
+                  <div className='d-flex justify-content-center'>
+                    <button type="button" className="btn btn-primary" onClick={handleResetPassword}>
+                      Reset Password
+                    </button>
+                  </div><br />
+                  <div className='d-flex justify-content-center'>
+                  <p style={{color:'red', fontSize:'larger'}}> Try to remember the password to avoid resetting the password</p>
+
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
